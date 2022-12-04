6267d336d520e1610f6ac398c8d02b3203ef5a79
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2022-12-04 18:39:30 +0300

    create script burger menu; add animation

--- script.js
+++ script.js
@@ -66,3 +66,14 @@ function onMenuLinkClick(e) {
     e.preventDefault();
   }
 }
+
+//burger menu open-close
+const iconMenu = document.querySelector(".menu__icon");
+if (iconMenu) {
+  const menuBody = document.querySelector(".menu__body");
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
