2133151f8340585ff40d74938c63ee4b7c3351e7
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2022-12-04 17:58:43 +0300

    add smoth scroll on page sections

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// detect touch screen and modified menu
 const isMobile = {
   Android: function () {
     return navigator.userAgent.match(/Android/i);
@@ -40,4 +41,28 @@ if (isMobile.any()) {
   }
 } else {
   document.body.classList.add("_pc");
+}
+
+//smooth scroll on menu click
+const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
+if (menuLinks) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener("click", onMenuLinkClick);
+  });
+}
+
+function onMenuLinkClick(e) {
+  const menuLink = e.target;
+  if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+    const gotoBlock = document.querySelector(menuLink.dataset.goto);
+    const gotoBlockValue =
+      gotoBlock.getBoundingClientRect().top +
+      scrollY -
+      document.querySelector(".header").offsetHeight;
+    window.scrollTo({
+      top: gotoBlockValue,
+      behavior: "smooth",
+    });
+    e.preventDefault();
+  }
 }
